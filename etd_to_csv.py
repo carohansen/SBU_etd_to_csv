355e5f29d2554a91d74619d748785eff5075a21a
Carolyn Hansen <37193656+carohansen@users.noreply.github.com>
2019-03-10 17:52:22 -0400

    Update etd_to_csv.py

--- etd_to_csv.py
+++ etd_to_csv.py
@@ -3,7 +3,7 @@ import csv
 import glob
 import re
 forCSV =[]
-forCSV.append(['authorname', 'title', 'dept', 'keywords', 'pages', 'abstract', 'advisorname', 'committeemembers', 'embargocode', 'filename'])
+forCSV.append(['authorname', 'title', 'dept', 'keywords', 'pages', 'abstract', 'advisorname', 'committeemembers', 'publishing_option', 'embargocode', 'third_party_search', 'delayed_release_date', 'complete_date', 'accept_date', 'filename'])
 
 for file in glob.glob('*.xml'):
 	print(file)
@@ -35,21 +35,29 @@ for file in glob.glob('*.xml'):
 	    combined_committee_members += fullname
 
 	for item in root.iter('DISS_description'):
-		title= item.find('DISS_title').text
+		accept_date= item.find('DISS_dates/DISS_accept_date').text
+		complete_date= item.find('DISS_dates/DISS_comp_date').text
 		dept= item.find('DISS_institution/DISS_inst_contact').text
 		keywords= item.find('DISS_categorization/DISS_keyword').text
 		pages= item.get('page_count') + " pages"
+		title= item.find('DISS_title').text
 
 	combined_abstracts= ""
 
 	for item in root.iter("DISS_para"):
 	    combined_abstracts += re.sub('\t', '', item.text)
 
+	for item in root.iter("DISS_repository"):
+		delayed_release_date= item.find('DISS_delayed_release').text
+
 	for item in root.iter('DISS_submission'):
 		embargocode= item.get('embargo_code')
+		publishing_option= item.get('publishing_option')
+		third_party_search= item.get('third_party_search')
 
-	forCSV.append([authorname, title, dept, keywords, pages, combined_abstracts, advisorname, combined_committee_members, embargocode, filename])
+	forCSV.append([authorname, title, dept, keywords, pages, combined_abstracts, advisorname, combined_committee_members, publishing_option, embargocode, third_party_search, delayed_release_date, complete_date, accept_date, filename])
 
 	with open('outputFile.csv', 'w', newline='') as csvfile:
 		wr= csv.writer(csvfile, delimiter=',')
 		wr.writerows(forCSV)
+
